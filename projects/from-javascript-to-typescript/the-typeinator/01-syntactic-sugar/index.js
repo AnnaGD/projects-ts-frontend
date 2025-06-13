768c9a59a58b1410ac8bab94eb476ee2585f4207
AnnaGD <anna.gonch.liaison@gmail.com>
2025-06-13 16:01:16 -0500

    Rewrote the function using modern JS syntax

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,21 @@
 // Put your announceMachines function here! ✨
+function announceMachines(announce, ...machines) {
+	let labelsCount = 0;
+
+	for (const machine of machines) {
+		const label = machines.label
+			? machines.label
+			: `Make: ${machine.make}; Model: ${machine.model}`;
+
+		if (machine.label) {
+			labelsCount += 1;
+		}
+
+		announce(label);
+	}
+	return labelsCount;
+}
+
 // See ./original.js for its older JavaScript code.
 
 module.exports.announceMachines = announceMachines;
